a8e3027526be82076b4f185bfc22a7f9c3db763a
Ian Del Giudice <ian.pvd@gmail.com>
2019-01-25 19:24:24 -0500

    Consolidate archive templates

--- archive.php
+++ archive.php
@@ -7,6 +7,18 @@
  * @package Gothamish
  */
 
+// Set up template variables.
+$title_class = 'page__title';
+
+if ( is_category() ) {
+	$title_class .= ' page__title--category';
+}
+
+if ( is_tag() ) {
+	// Retreive object for currently querried tag.
+	$tag_archive = get_queried_object();
+}
+
 get_header();
 ?>
 
@@ -14,9 +26,14 @@ get_header();
 		<main id="main" class="site-main">
 
 			<header class="page__header page__header--archive">
-				<?php the_archive_title( '<h1 class="page__title page__title--category">', '</h1>' ); ?>
+				<?php
+				if ( is_tag() ) {
+					gotham_tag_banner( $tag_archive->slug );
+				}
 
-				<?php the_archive_description( '<div class="page__description">', '</div>' ); ?>
+				the_archive_title( '<h1 class=" ' . $title_class . ' ">', '</h1>' );
+				the_archive_description( '<div class="page__description">', '</div>' );
+				?>
 			</header><!-- .page-header -->
 
 			<div class="post-list">
