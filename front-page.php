fe2e0cf92540a5367f4d766ad4c1f0cf818de805
Ian Del Giudice <ian.pvd@gmail.com>
2019-02-01 16:06:33 -0500

    Add front page template

---
+++ front-page.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * The front page template file
+ *
+ * Template Name: Front Page
+ *
+ * @package Gothamish
+ */
+
+get_header();
+?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+
+			<div>HERO SECTION
+				<div>POST GRID</div>
+				<div>HERO SIDEBAR
+					<div>AD PLACEHOLDER</div>
+					<div>SUBSCRIBE WIDGET</div>
+					<DIV>QUICK LINKS</DIV>
+				</div>
+			</div>
+
+			<div>POSTS FEED</div>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_footer();
