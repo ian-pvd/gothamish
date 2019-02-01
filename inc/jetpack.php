a0af6436e66a714c5f62c6de1b5e1dd696323087
Ian Del Giudice <ian.pvd@gmail.com>
2019-02-01 14:01:41 -0500

    Fix: jetpack content customizer options

--- inc/jetpack.php
+++ inc/jetpack.php
@@ -16,31 +16,38 @@
  */
 function gotham_jetpack_setup() {
 	// Add theme support for Infinite Scroll.
-	add_theme_support( 'infinite-scroll', array(
-		'container' => 'main',
-		'render'    => 'gotham_infinite_scroll_render',
-		'footer'    => 'page',
-	) );
+	add_theme_support(
+		'infinite-scroll',
+		[
+			'container' => 'main',
+			'render'    => 'gotham_infinite_scroll_render',
+			'footer'    => 'page',
+		]
+	);
 
 	// Add theme support for Responsive Videos.
 	add_theme_support( 'jetpack-responsive-videos' );
 
 	// Add theme support for Content Options.
-	add_theme_support( 'jetpack-content-options', array(
-		'post-details'    => array(
-			'stylesheet' => 'gotham-style',
-			'date'       => '.posted-on',
-			'categories' => '.cat-links',
-			'tags'       => '.tags-links',
-			'author'     => '.byline',
-			'comment'    => '.comments-link',
-		),
-		'featured-images' => array(
-			'archive'    => true,
-			'post'       => true,
-			'page'       => true,
-		),
-	) );
+	add_theme_support(
+		'jetpack-content-options',
+		[
+			'post-details'    =>
+				[
+					'stylesheet' => 'gotham-post-css',
+					'date'       => '.entry-date',
+					'categories' => '.entry-category',
+					'tags'       => '.entry-footer__tags',
+					'author'     => '.byline',
+				],
+			'featured-images' =>
+				[
+					'archive' => true,
+					'post'    => true,
+					'page'    => true,
+				],
+		]
+	);
 }
 add_action( 'after_setup_theme', 'gotham_jetpack_setup' );
 
@@ -53,7 +60,7 @@ function gotham_infinite_scroll_render() {
 		if ( is_search() ) :
 			get_template_part( 'template-parts/content', 'search' );
 		else :
-			get_template_part( 'template-parts/content', get_post_type() );
+			get_template_part( 'template-parts/content' );
 		endif;
 	}
 }
@@ -61,12 +68,14 @@ function gotham_infinite_scroll_render() {
 /**
  * Customize Jetpack Sharedaddy links.
  */
-function jptweak_remove_share() {
+function gotham_jetpack_sharing() {
+
 	remove_filter( 'the_content', 'sharing_display', 19 );
 	remove_filter( 'the_excerpt', 'sharing_display', 19 );
+
 	if ( class_exists( 'Jetpack_Likes' ) ) {
 		remove_filter( 'the_content', array( Jetpack_Likes::init(), 'post_likes' ), 30, 1 );
 	}
 }
-add_action( 'loop_start', 'jptweak_remove_share' );
+add_action( 'loop_start', 'gotham_jetpack_sharing' );
 
