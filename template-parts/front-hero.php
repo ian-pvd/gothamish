354721e771cad01750bd4c549e17e348cd80dc40
Ian Del Giudice <ian.pvd@gmail.com>
2019-02-05 17:17:47 -0500

    v1 front featured posts

--- template-parts/front-hero.php
+++ template-parts/front-hero.php
@@ -7,13 +7,35 @@
  * @package Gothamish
  */
 
+/**
+ * Featured posts function.
+ *
+ * @return mixed Featured posts.
+ */
+function gotham_featured_posts() {
+	// Featured posts args.
+	$featured_posts = new WP_Query(
+		[
+			'posts_per_page' => 3,
+			'no_found_rows'  => true,
+		]
+	);
+
+	if ( ! $featured_posts->have_posts() ) {
+		return;
+	}
+
+	// Widget contents, loop of "best of" posts.
+	while ( $featured_posts->have_posts() ) :
+		$featured_posts->the_post();
+			get_template_part( 'template-parts/content', 'tout' );
+	endwhile;
+}
 ?>
 
 <div class="front-page__hero-section hero-section">
 	<div class="hero-section__featured-posts featured-posts">
-			<article class="post">POST 1</article>
-			<article class="post">POST 3</article>
-			<article class="post">POST 2</article>
+			<?php gotham_featured_posts(); ?>
 	</div>
 	<?php get_sidebar(); ?>
 </div>
