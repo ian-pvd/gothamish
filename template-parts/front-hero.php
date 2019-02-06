cf760c5cb25a4ec80e5ee159ee1e24803ad49809
Ian Del Giudice <ian.pvd@gmail.com>
2019-02-05 20:12:43 -0500

    Add featured posts to home page via merge array.

--- template-parts/front-hero.php
+++ template-parts/front-hero.php
@@ -13,23 +13,61 @@
  * @return mixed Featured posts.
  */
 function gotham_featured_posts() {
-	// Featured posts args.
-	$featured_posts = new WP_Query(
-		[
-			'posts_per_page' => 3,
-			'no_found_rows'  => true,
-		]
-	);
+	// Define a featured posts global.
+	global $featured_post;
 
-	if ( ! $featured_posts->have_posts() ) {
-		return;
+	// TODO: Check for cached featured posts.
+	// First, check for Jetpack featured posts.
+	if ( get_theme_support( 'featured-content' ) ) {
+		// Get Featured Content from Jetpack.
+		$jetpack_featured = apply_filters( 'gotham_get_featured_posts', [] );
+	} else {
+		$jetpack_featured = [];
 	}
 
-	// Widget contents, loop of "best of" posts.
-	while ( $featured_posts->have_posts() ) :
-		$featured_posts->the_post();
-			get_template_part( 'template-parts/content', 'tout' );
-	endwhile;
+	// Determine what to do with the results.
+	if ( count( $jetpack_featured ) < 3 ) {
+
+		// Collect IDs to exclude duplicates.
+		$duplicate_ids = [];
+
+		// If no jetpack featured, or less than 3 found, check for more.
+		if ( ! empty( $jetpack_featured ) ) {
+			foreach ( $jetpack_featured as $key => $post_object ) {
+				$duplicate_ids[] = $post_object->ID;
+			}
+		}
+
+		// Featured posts args.
+		$recent_posts = new WP_Query(
+			[
+				'no_found_rows'  => true,
+				'post__not_in'   => $duplicate_ids,
+				'posts_per_page' => 3,
+			]
+		);
+
+		// Shift featured posts query to array.
+		$recent_posts = $recent_posts->posts;
+	} else {
+		$recent_posts = [];
+	}
+
+	// Merge the results down to three posts.
+	$featured_posts = array_slice( array_merge( $jetpack_featured, $recent_posts ), 0, 3);
+
+	// TODO: Stash the featured posts.
+
+	// Display the posts.
+	foreach ( $featured_posts as $featured_post ) {
+		get_template_part( 'template-parts/content', 'featured' );
+	}
+
+	// Loop through featured posts with the tout template.
+	// while ( $featured_posts->have_posts() ) :
+	// 	$featured_posts->the_post();
+
+	// endwhile;
 }
 ?>
 
