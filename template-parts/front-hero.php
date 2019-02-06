14631df3fe6bcaf4f83e5716d14ac803db252f0d
Ian Del Giudice <ian.pvd@gmail.com>
2019-02-05 20:41:09 -0500

    WIP get featured posts & backfill

--- template-parts/front-hero.php
+++ template-parts/front-hero.php
@@ -13,9 +13,6 @@
  * @return mixed Featured posts.
  */
 function gotham_featured_posts() {
-	// Define a featured posts global.
-	global $featured_post;
-
 	// TODO: Check for cached featured posts.
 	// First, check for Jetpack featured posts.
 	if ( get_theme_support( 'featured-content' ) ) {
@@ -54,20 +51,31 @@ function gotham_featured_posts() {
 	}
 
 	// Merge the results down to three posts.
-	$featured_posts = array_slice( array_merge( $jetpack_featured, $recent_posts ), 0, 3);
+	$merged_posts = array_slice( array_merge( $jetpack_featured, $recent_posts ), 0, 3 );
+	// Put those post IDs into an array.
+	if ( ! empty( $merged_posts ) ) {
+		foreach ( $merged_posts as $key => $post_object ) {
+			$featured_ids[] = $post_object->ID;
+		}
+	}
+
+	// Featured posts args.
+	$featured_posts = new WP_Query(
+		[
+			'no_found_rows'  => true,
+			'post__in'       => $featured_ids,
+			'posts_per_page' => 3,
+			'orderby'        => 'post__in',
+		]
+	);
 
 	// TODO: Stash the featured posts.
 
-	// Display the posts.
-	foreach ( $featured_posts as $featured_post ) {
-		get_template_part( 'template-parts/content', 'featured' );
-	}
-
 	// Loop through featured posts with the tout template.
-	// while ( $featured_posts->have_posts() ) :
-	// 	$featured_posts->the_post();
-
-	// endwhile;
+	while ( $featured_posts->have_posts() ) :
+		$featured_posts->the_post();
+		get_template_part( 'template-parts/content', 'tout' );
+	endwhile;
 }
 ?>
 
