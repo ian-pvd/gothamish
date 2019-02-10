2d01209c4794c0f554044035f4876e0b2a113852
Ian Del Giudice <ian.pvd@gmail.com>
2019-02-10 18:39:41 -0500

    MVP recirculation section, donate appeal, styles cleanup

--- front-page.php
+++ front-page.php
@@ -38,6 +38,41 @@ get_header();
 
 		<div id="network-feed" class="content-area content-area--front-recirc">
 			<?php dynamic_sidebar( 'front-recirc' ); ?>
+			<section class="front-recirc__donation-appeal">
+				<?php
+					printf(
+						wp_kses(
+							/* translators: 1: blog name, 2: site locale */
+							__(
+								'<p class="donation-appeal__text">%1$s is reader supported local news. Help us keep telling %2$s stories.</p>',
+								'gotham'
+							),
+							[
+								'span' => [
+									'class' => [],
+								],
+								'p' => [
+									'class' => [],
+								],
+							]
+						),
+						wp_kses(
+							gotham_logotype(),
+							[
+								'span' => [
+									'class' => [],
+								],
+							]
+						),
+						esc_html( gotham_get_option( 'site-location', 'New York' ) )
+					);
+
+					printf(
+						'<a href="' . esc_url( '/donate' ) . '" class="link-button">%s</a>',
+						esc_html__( 'Support Us!', 'gotham' )
+					);
+				?>
+			</section>
 		</div>
 
 	<?php endif; ?>
