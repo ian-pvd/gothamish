2ca95063a16f3873d48fa9437b387c8395c76a11
Ian Del Giudice <ian.pvd@gmail.com>
2019-02-14 18:48:40 -0500

    WIP front featured contnet blocks

--- inc/front-featured/index.php
+++ inc/front-featured/index.php
@@ -11,5 +11,71 @@ require_once GOTHAM_PATH . '/inc/front-featured/featured-posts.php';
 // Load Front Featured Category Feeds module.
 require_once GOTHAM_PATH . '/inc/front-featured/featured-categories-feed.php';
 
-// Load Front Featued Content cache reset hook.
+// Load Front Featured Content cache reset hook.
 require_once GOTHAM_PATH . '/inc/front-featured/delete-front-featured-cache.php';
+
+
+function gotham_front_featured_block( $block_number = 0 ) {
+	// Block Config: Featured.
+	$blocks[] = [
+		'slug' => 'gothamist-featured',
+		'title' => 'Featured on Gothamish',
+		'query_args' => [
+			'tag' => 'featured',
+		],
+	];
+	// Block Config: Long Form.
+	$blocks[] = [
+		'slug' => 'gothamist-long-form',
+		'title' => 'Long Form on Gothamish',
+		'query_args' => [
+			'tag' => 'long-form',
+		],
+	];
+	// Block Config: Best Of.
+	$blocks[] = [
+		'slug' => 'gothamist-best-of',
+		'title' => 'Best of Gothamish',
+		'query_args' => [
+			'tag' => 'best-of',
+		],
+	];
+
+	// Get the block number (remaineder of page_num/total_blocks).
+	$block_number--;
+	$block_number = ( $block_number % count( $blocks ) );
+
+	// Shift block config to $block variable.
+	$block = $blocks[ $block_number ];
+
+	// Default blocks args.
+	$blocks_args = [
+		'posts_per_page'      => 3,
+		'ignore_sticky_posts' => true,
+	];
+
+	// Merge featured block custom query.
+	$blocks_args = array_merge( $blocks_args, $block['query_args'] );
+
+	$block_posts[ $block['slug'] ] = new WP_Query( $blocks_args );
+
+	if ( $block_posts[ $block['slug'] ]->have_posts() ) :
+		?>
+		<div class="featured-block featured-block--<?php echo esc_attr( $block['slug'] ); ?>">
+			<h2 class="featured-block__title"><?php echo $block['title']; ?></h2>
+			<div class="featured-block__wrapper">
+
+			<?php
+			while ( $block_posts[ $block['slug'] ]->have_posts() ) {
+				$block_posts[ $block['slug'] ]->the_post();
+				get_template_part( 'template-parts/content', 'tout' );
+			}
+
+			?>
+			</div>
+		</div>
+		<?php
+	endif;
+
+	unset( $block_posts[ $block['slug'] ] );
+}
--- template-parts/front-primary-feed.php
+++ template-parts/front-primary-feed.php
@@ -10,6 +10,9 @@
 // Current Primary Feed page.
 $primary_feed_page = 1;
 
+// Max number of feed pages to display.
+$max_feed_pages = 20; // DEV get_option( 'posts_per_page' );
+
 // Get primary categories from theme options.
 $primary_categories = gotham_get_option( 'primary-categories' );
 
@@ -22,7 +25,7 @@ foreach ( $primary_categories as $key ) {
 	$more_posts[ $key ] = true;
 }
 
-while ( $primary_feed_page <= 10 && ! empty( $more_posts ) ) :
+while ( $primary_feed_page <= $max_feed_pages && ! empty( $more_posts ) ) :
 	?>
 
 	<div class="primary-feed__page primary-feed__page--<?php echo esc_attr( str_pad( $primary_feed_page, 3, '0', STR_PAD_LEFT ) ); ?>">
@@ -31,60 +34,55 @@ while ( $primary_feed_page <= 10 && ! empty( $more_posts ) ) :
 			foreach ( $primary_categories as $category ) :
 				$category = get_category_by_slug( $category );
 
-				$primary_feed[ $category->slug ] = new WP_Query(
-					[
-						'post__not_in'  => $exclude_displayed_post_ids,
-						'category_name' => $category->slug,
-						'paged'         => $primary_feed_page,
-					]
-				);
+				if ( isset( $more_posts[ $category->slug ] ) ) {
+					$primary_feed[ $category->slug ] = new WP_Query(
+						[
+							'post__not_in'  => $exclude_displayed_post_ids,
+							'category_name' => $category->slug,
+							'paged'         => $primary_feed_page,
+						]
+					);
+				}
 				?>
 
-			<div class="post-feed__column">
-				<h2 class="post-feed__category-title">
-					<a href="<?php echo esc_url( get_category_link( $category->term_id ) ); ?>" title="<?php echo esc_attr( $category->name ); ?>">
-						<?php echo esc_html( $category->name ); ?>
-					</a>
-				</h2>
-				<?php
-				if ( $primary_feed[ $category->slug ]->have_posts() ) {
-					while ( $primary_feed[ $category->slug ]->have_posts() ) {
-						$primary_feed[ $category->slug ]->the_post();
-						get_template_part( 'template-parts/content', 'tout' );
+				<div class="post-feed__column">
+					<h2 class="post-feed__category-title">
+						<a href="<?php echo esc_url( get_category_link( $category->term_id ) ); ?>" title="<?php echo esc_attr( $category->name ); ?>">
+							<?php echo esc_html( $category->name ); ?>
+						</a>
+					</h2>
+					<?php
+					if ( isset( $primary_feed[ $category->slug ] ) && $primary_feed[ $category->slug ]->have_posts() ) {
+						while ( $primary_feed[ $category->slug ]->have_posts() ) {
+							$primary_feed[ $category->slug ]->the_post();
+							get_template_part( 'template-parts/content', 'tout' );
+						}
+
+						// If this page is the max # pages...
+						if ( (int) $primary_feed[ $category->slug ]->max_num_pages === $primary_feed_page ) {
+							// Destroy "more posts?" value.
+							unset( $more_posts[ $category->slug ] );
+						}
+
+						// Clean up this page of primary feed posts.
+						unset( $primary_feed[ $category->slug ] );
+					} else {
+						// And that's why you always leave a note.
+						echo '<!-- No more posts for this category. -->';
 					}
-				}
-				?>
-			</div>
-				<?php
-				// If this page is the max # pages...
-				if ( (int) $primary_feed[ $category->slug ]->max_num_pages === $primary_feed_page ) {
-					// Destroy "more posts?" value.
-					unset( $more_posts[ $category->slug ] );
-				}
-			endforeach;
-			?>
+					?>
+				</div>
+
+			<?php endforeach; ?>
 		</div>
 
-		<div class="featured-block featured-block--gothamist-featured">
-			<h2 class="featured-block__title">Featured on <?php echo gotham_logotype(); ?></h2>
-			<div class="featured-block__wrapper">
-			<?php
-			$featured_block['gothamist-featured'] = new WP_Query(
-				[
-					'posts_per_page'      => 3,
-					'ignore_sticky_posts' => true,
-				]
-			);
-
-			if ( $featured_block['gothamist-featured']->have_posts() ) {
-				while ( $featured_block['gothamist-featured']->have_posts() ) {
-					$featured_block['gothamist-featured']->the_post();
-					get_template_part( 'template-parts/content', 'tout' );
-				}
-			}
-			?>
-			</div>
-		</div>
+		<?php
+		// If there are still posts.
+		if ( ! empty( $more_posts ) ) {
+			// Display a Featured Content Block.
+			gotham_front_featured_block( $primary_feed_page );
+		}
+		?>
 
 	</div>
 
