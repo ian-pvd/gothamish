aa84a9324379f563cdfc4dbec1fc80afe8c11ec7
Ian Del Giudice <ian.pvd@gmail.com>
2019-02-19 22:48:59 -0500

    Use theme content template for blog feed.

--- index.php
+++ index.php
@@ -38,11 +38,9 @@ get_header();
 					 * If you want to override this in a child theme, then include a file
 					 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
 					 */
-					get_template_part( 'template-parts/content', get_post_type() );
+					get_template_part( 'template-parts/content' );
 
 				endwhile;
-
-				the_posts_navigation();
 
 			else :
 
@@ -52,6 +50,8 @@ get_header();
 			?>
 
 			</div><!-- .post-area -->
+
+			<?php the_posts_pagination(); ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
